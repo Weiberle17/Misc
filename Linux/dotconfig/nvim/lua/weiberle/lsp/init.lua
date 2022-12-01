142af63fa43940d28329433e7bffeba32e5765c6
Weiberle17 <maxweiberle63@gmail.com>
2022-12-01 22:51:05 +0100

    moved files to dotfiles repo

--- Linux/dotconfig/nvim/lua/weiberle/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "weiberle.lsp.configs"
-require("weiberle.lsp.handlers").setup()
-require "weiberle.lsp.null-ls"
